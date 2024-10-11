9975a3beb1ac6df7dc30cfb162ecc9b5d91e09c2
densematcher <densematcher@densematcher.com>
2024-10-12 05:52:33 +0800

    make videos also interactive buttongs

--- static/js/render.js
+++ static/js/render.js
@@ -1,138 +1,3 @@
-// //Import the THREE.js library
-// import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-// // To allow for the camera to move around the scene
-// import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-// // To allow for importing the .gltf file
-// import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
-// import { OBJLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/OBJLoader.js";
-
-
-// //Set which object to render
-// let objToRender = 'dino';
-
-
-// let list = [
-//   "1d6d1_toy_animals_015", "2d6b3_toy_animals_009", "34fb4_toy_animals_019",
-//   "2a535_toy_truck_002", "35ebd_toy_car_022", "6bb21_toy_bus_041", 
-//   "apple_19", "apple_44", "apple_42",
-//   "banana_84", "banana_98", "banana_133"
-// ];  // Add more folder names as needed
-// const list_assets = list.flatMap(folder => [
-//   { type: "annotation", path: `static/meshes/${folder}/annotation.obj` },
-//   { type: "color", path: `static/meshes/${folder}/color_mesh.obj` }
-// ]);
-// list_assets.forEach((asset, index) => {
-//   if (asset.type === 'color') {
-//     return;
-//   }
-  
-//   //Create a Three.JS Scene
-//   const scene = new THREE.Scene();
-//   //create a new camera with positions and angles
-//   // const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-//   const camera = new THREE.PerspectiveCamera(75, 1 / 0.75, 0.1, 1000);
-
-
-//   //Keep track of the mouse position, so we can make the eye move
-//   let mouseX = window.innerWidth / 2;
-//   let mouseY = window.innerHeight / 2;
-
-//   //Keep the 3D object on a global variable so we can access it later
-//   let object;
-
-//   //OrbitControls allow the camera to move around the scene
-//   let controls;
-
-//   //Instantiate a loader for the .gltf file
-//   // const loader = new GLTFLoader();
-//   const loader = new OBJLoader();
-//   const containerId = `container_${asset.type}${Math.floor(index / 2)}`;
-
-//   //Load the file
-//   loader.load(
-//     asset.path,
-//     function ( obj ) {
-//       object = obj
-//       scene.add( obj );
-
-//     },
-//     function (xhr) {
-//       //While it is loading, log the progress
-//       console.log(`${asset.path} ${(xhr.loaded / xhr.total * 100)}% loaded`);
-//     },
-//     function (error) {
-//       //If there is an error, log it
-//       console.error(`Error loading ${asset.path}:`, error);
-//     }
-//   );
-
-//   //Instantiate a new renderer and set its size
-//   const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
-//   // renderer.setSize(window.innerWidth, window.innerHeight);
-//   renderer.setSize(window.innerWidth, window.innerWidth * 0.75);
-
-  // console.log(`Filling container ${containerId}`);
-  // //Add the renderer to the DOM
-  // document.getElementById(containerId).appendChild(renderer.domElement);
-
-  // //Set how far the camera will be from the 3D model
-  // // camera.position.z = objToRender === "dino" ? 25 : 500;
-  // camera.position.z = 0.25;
-
-  // //Add lights to the scene, so we can actually see the 3D model
-  // const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-  // topLight.position.set(500, 500, 500) //top-left-ish
-  // topLight.castShadow = false;
-  // scene.add(topLight);
-
-  // const frontLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-  // frontLight.position.set(-500, -500, -500) //top-left-ish
-  // frontLight.castShadow = false;
-  // scene.add(frontLight);
-
-  // // const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
-  // const ambientLight = new THREE.AmbientLight(0x333333, 1);
-  // scene.add(ambientLight);
-
-//   //This adds controls to the camera, so we can rotate / zoom it with the mouse
-//   if (objToRender === "dino") {
-//     controls = new OrbitControls(camera, renderer.domElement);
-//   }
-
-//   //Render the scene
-//   function animate() {
-//     requestAnimationFrame(animate);
-//     //Here we could add some code to update the scene, adding some automatic movement
-
-//     //Make the eye move
-//     if (object && objToRender === "eye") {
-//       //I've played with the constants here until it looked good 
-//       object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-//       object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
-//     }
-//     renderer.render(scene, camera);
-
-//   }
-
-//   //Add a listener to the window, so we can resize the window and the camera
-//   window.addEventListener("resize", function () {
-//     // camera.aspect = window.innerWidth / window.innerHeight;
-//     camera.aspect = 1 / 0.75;
-//     camera.updateProjectionMatrix();
-//     // renderer.setSize(window.innerWidth, window.innerHeight);
-//     renderer.setSize(window.innerWidth, window.innerWidth * 0.75);
-//   });
-
-//   //add mouse position listener, so we can make the eye move
-//   document.onmousemove = (e) => {
-//     mouseX = e.clientX;
-//     mouseY = e.clientY;
-//   }
-
-//   //Start the 3D rendering
-//   animate();
-// });
-
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 // To allow for the camera to move around the scene
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
@@ -168,9 +33,6 @@ function load3DModel(asset, index) {
 
   // Keep the 3D object on a global variable so we can access it later
   let object;
-
-  // OrbitControls allow the camera to move around the scene
-  const controls = new OrbitControls(camera, document.getElementById(`container_annotation${index}`));
 
   // Instantiate a loader for the .obj file
   const loader = new OBJLoader();
@@ -219,12 +81,24 @@ function load3DModel(asset, index) {
   const ambientLight = new THREE.AmbientLight(0x333333, 1);
   scene.add(ambientLight);
   
+
+  // OrbitControls allow the camera to move around the scene
+  const controls = new OrbitControls(camera, document.getElementById(`container_annotation${index}`));
+
   // Animation loop
   function animate() {
     requestAnimationFrame(animate);
     controls.update();
     renderer.render(scene, camera);
   }
+
+  window.addEventListener("resize", function () {
+    // camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = 1 / 0.75;
+    camera.updateProjectionMatrix();
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerWidth * 0.75);
+  });
   animate();
 }
 
---
+++ static/js/switch_videos.js
@@ -0,0 +1,280 @@
+// // Function to load videos or images into placeholders
+// function loadContent(humanContent1, humanContent2, robotContent, caption, isImage = false) {
+//     const humanVideoPlaceholder1 = document.getElementById('human-video-placeholder-1');
+//     const humanVideoPlaceholder2 = document.getElementById('human-video-placeholder-2');
+//     const robotVideoPlaceholder = document.getElementById('robot-video-placeholder');
+//     const taskCaption = document.getElementById('task-caption');
+  
+//     // Clear existing content
+//     humanVideoPlaceholder1.innerHTML = '';
+//     humanVideoPlaceholder2.innerHTML = '';
+//     robotVideoPlaceholder.innerHTML = '';
+  
+//     if (isImage) {
+//       const templateLabel = document.createElement('h6');
+//       templateLabel.textContent = 'Template';
+//       humanVideoPlaceholder1.appendChild(templateLabel);
+    
+//       // Create and append human images
+//       const humanImage1 = document.createElement('img');
+//       humanImage1.setAttribute('src', humanContent1);
+//       humanImage1.setAttribute('width', 'auto');
+//       humanVideoPlaceholder1.appendChild(humanImage1);
+
+//       const targetLabel = document.createElement('h6');
+//       targetLabel.textContent = 'Target';
+//       humanVideoPlaceholder2.appendChild(targetLabel);
+    
+//       const humanImage2 = document.createElement('img');
+//       humanImage2.setAttribute('src', humanContent2);
+//       humanImage2.setAttribute('width', 'auto');
+//       humanVideoPlaceholder2.appendChild(humanImage2);
+  
+//       // Create and append robot video
+//       const robotVideo = document.createElement('video');
+//       robotVideo.setAttribute('controls', '');
+//       robotVideo.setAttribute('playsinline', '');
+//       robotVideo.setAttribute('height', 'auto');
+//       const robotSource = document.createElement('source');
+//       robotSource.setAttribute('src', robotContent);
+//       robotSource.setAttribute('type', 'video/mp4');
+//       robotVideo.appendChild(robotSource);
+//       robotVideoPlaceholder.appendChild(robotVideo);
+//     } else {
+//       // Create and append human videos
+//       const humanVideo1 = document.createElement('video');
+//       humanVideo1.setAttribute('autoplay', '');
+//       humanVideo1.setAttribute('controls', '');
+//       humanVideo1.setAttribute('muted', '');
+//       humanVideo1.setAttribute('loop', '');
+//       humanVideo1.setAttribute('playsinline', '');
+//       humanVideo1.setAttribute('height', 'auto');
+//       const humanSource1 = document.createElement('source');
+//       humanSource1.setAttribute('src', humanContent1);
+//       humanSource1.setAttribute('type', 'video/mp4');
+//       humanVideo1.appendChild(humanSource1);
+//       humanVideoPlaceholder1.appendChild(humanVideo1);
+  
+//         // Check if humanContent2 is not null before creating the second human video
+//         if (humanContent2 !== null) {
+//             const humanVideo2 = document.createElement('video');
+//             humanVideo2.setAttribute('autoplay', '');
+//             humanVideo2.setAttribute('controls', '');
+//             humanVideo2.setAttribute('muted', '');
+//             humanVideo2.setAttribute('loop', '');
+//             humanVideo2.setAttribute('playsinline', '');
+//             humanVideo2.setAttribute('height', 'auto');
+//             const humanSource2 = document.createElement('source');
+//             humanSource2.setAttribute('src', humanContent2);
+//             humanSource2.setAttribute('type', 'video/mp4');
+//             humanVideo2.appendChild(humanSource2);
+//             humanVideoPlaceholder2.appendChild(humanVideo2);
+//         }
+        
+//       // Create and append robot video
+//       const robotVideo = document.createElement('video');
+//       robotVideo.setAttribute('controls', '');
+//       robotVideo.setAttribute('playsinline', '');
+//       robotVideo.setAttribute('height', 'auto');
+//       const robotSource = document.createElement('source');
+//       robotSource.setAttribute('src', robotContent);
+//       robotSource.setAttribute('type', 'video/mp4');
+//       robotVideo.appendChild(robotSource);
+//       robotVideoPlaceholder.appendChild(robotVideo);
+//     }
+  
+//     // Set the task caption
+//     taskCaption.innerHTML = caption;
+//   }
+  
+//   // Event listeners for the buttons
+//   document.getElementById('load-banana').addEventListener('click', () => {
+//     loadContent(
+//       './static/our_videos/human_video/new_banana.mp4',
+//       null,
+//       './static/our_videos/robot_video/new_banana_robot1.mp4',
+//       'Peeling a Banana'
+//     );
+//   });
+  
+//   document.getElementById('load-carrot').addEventListener('click', () => {
+//     loadContent(
+//       './static/our_videos/human_video/new_carrot.mp4',
+//       './static/our_videos/human_video/new_kettle.mp4',
+//       './static/our_videos/robot_video/new_Carrot_robot_1_2x.mp4',
+//       'Watering Carrots'
+//     );
+//   });
+  
+//   document.getElementById('load-flower').addEventListener('click', () => {
+//     loadContent(
+//       './static/our_videos/human_video/new_flower_human_video.mp4',
+//       null,
+//       './static/our_videos/robot_video/new_flower_robot_1.mp4',
+//       'Putting Flowers into a Vase'
+//     );
+//   });
+  
+//   document.getElementById('load-tree').addEventListener('click', () => {
+//     loadContent(
+//       './static/our_videos/human_video/new_tree.mp4',
+//       null,
+//       './static/our_videos/robot_video/new_Christmas_robot1.mp4',
+//       'Decorating a Christmas Tree'
+//     );
+//   });
+  
+//   document.getElementById('load-shoes').addEventListener('click', () => {
+//     loadContent(
+//       './static/our_videos/human_video/new_shoes.mp4',
+//       null,
+//       './static/our_videos/robot_video/new_shoes_robot_1.mp4',
+//       'Organizing Shoes'
+//     );
+//   });
+  
+//   document.getElementById('load-dog').addEventListener('click', () => {
+//     loadContent(
+//       './static/our_videos/dog/color.png',
+//       './static/our_videos/dog/color2.png',
+//       './static/our_videos/robot_video/new_long_5x.mp4',
+//       'Transferring Pre-labelled Keypoints to New Objects and Poking Them with a Pen',
+//       true
+//     );
+//   });
+
+function loadContent(humanContent1, humanContent2, robotContent, caption, isImage = false) {
+    const leftColumn = document.getElementById('left-videos');
+    const robotVideoPlaceholder = document.getElementById('robot-video-placeholder');
+    const taskCaption = document.getElementById('task-caption');
+  
+    // Clear existing content
+    leftColumn.innerHTML = '';
+    robotVideoPlaceholder.innerHTML = '';
+
+
+    if (isImage) {
+        // Add labels and videos/images to the left-videos column
+        const templateLabel = document.createElement('h6');
+        templateLabel.textContent = 'Template';
+        const humanImage1 = document.createElement('img');
+        humanImage1.setAttribute('src', humanContent1);
+        humanImage1.setAttribute('width', '100%');
+        humanImage1.setAttribute('height', 'auto');
+        leftColumn.appendChild(humanImage1);
+        leftColumn.appendChild(templateLabel);
+    } else {
+        const humanVideo1 = document.createElement('video');
+        humanVideo1.setAttribute('autoplay', '');
+        humanVideo1.setAttribute('controls', '');
+        humanVideo1.setAttribute('muted', '');
+        humanVideo1.setAttribute('loop', '');
+        humanVideo1.setAttribute('playsinline', '');
+        humanVideo1.setAttribute('width', '100%');
+        humanVideo1.setAttribute('height', 'auto');
+        const humanSource1 = document.createElement('source');
+        humanSource1.setAttribute('src', humanContent1);
+        humanSource1.setAttribute('type', 'video/mp4');
+        humanVideo1.appendChild(humanSource1);
+        leftColumn.appendChild(humanVideo1);
+    }
+
+    if (humanContent2 !== null) {
+
+        if (isImage) {
+            const targetLabel = document.createElement('h6');
+            targetLabel.textContent = 'Target';
+            const humanImage2 = document.createElement('img');
+            humanImage2.setAttribute('src', humanContent2);
+            humanImage2.setAttribute('width', '100%');
+            humanImage2.setAttribute('height', 'auto');
+            leftColumn.appendChild(humanImage2);
+            leftColumn.appendChild(targetLabel);
+        } else {
+            const humanVideo2 = document.createElement('video');
+            humanVideo2.setAttribute('autoplay', '');
+            humanVideo2.setAttribute('controls', '');
+            humanVideo2.setAttribute('muted', '');
+            humanVideo2.setAttribute('loop', '');
+            humanVideo2.setAttribute('playsinline', '');
+            humanVideo2.setAttribute('width', '100%');
+            humanVideo2.setAttribute('height', 'auto');
+            const humanSource2 = document.createElement('source');
+            humanSource2.setAttribute('src', humanContent2);
+            humanSource2.setAttribute('type', 'video/mp4');
+            humanVideo2.appendChild(humanSource2);
+            leftColumn.appendChild(humanVideo2);
+        }
+    }
+
+    // Create and append robot video
+    const robotVideo = document.createElement('video');
+    robotVideo.setAttribute('controls', '');
+    robotVideo.setAttribute('playsinline', '');
+    robotVideo.setAttribute('width', '100%');
+    robotVideo.setAttribute('height', 'auto');
+    const robotSource = document.createElement('source');
+    robotSource.setAttribute('src', robotContent);
+    robotSource.setAttribute('type', 'video/mp4');
+    robotVideo.appendChild(robotSource);
+    robotVideoPlaceholder.appendChild(robotVideo);
+
+    // Set the task caption
+    taskCaption.innerHTML = caption;
+}
+
+  // Event listeners for the buttons
+  document.getElementById('load-banana').addEventListener('click', () => {
+    loadContent(
+      './static/our_videos/human_video/new_banana.mp4',
+      null,
+      './static/our_videos/robot_video/new_banana_robot1.mp4',
+      'Peeling a Banana'
+    );
+  });
+  
+  document.getElementById('load-carrot').addEventListener('click', () => {
+    loadContent(
+      './static/our_videos/human_video/new_carrot.mp4',
+      './static/our_videos/human_video/new_kettle.mp4',
+      './static/our_videos/robot_video/new_Carrot_robot_1_2x.mp4',
+      'Watering Carrots'
+    );
+  });
+  
+  document.getElementById('load-flower').addEventListener('click', () => {
+    loadContent(
+      './static/our_videos/human_video/new_flower_human_video.mp4',
+      null,
+      './static/our_videos/robot_video/new_flower_robot_1.mp4',
+      'Putting Flowers into a Vase'
+    );
+  });
+  
+  document.getElementById('load-tree').addEventListener('click', () => {
+    loadContent(
+      './static/our_videos/human_video/new_tree.mp4',
+      null,
+      './static/our_videos/robot_video/new_Christmas_robot1.mp4',
+      'Decorating a Christmas Tree'
+    );
+  });
+  
+  document.getElementById('load-shoes').addEventListener('click', () => {
+    loadContent(
+      './static/our_videos/human_video/new_shoes.mp4',
+      null,
+      './static/our_videos/robot_video/new_shoes_robot_1.mp4',
+      'Organizing Shoes'
+    );
+  });
+  
+  document.getElementById('load-dog').addEventListener('click', () => {
+    loadContent(
+      './static/our_videos/dog/color.png',
+      './static/our_videos/dog/color2.png',
+      './static/our_videos/robot_video/new_long_5x.mp4',
+      'Transferring Pre-labelled Keypoints to New Objects and Poking Them with a Pen',
+      true
+    );
+  });
